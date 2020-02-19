997e88f067c81a2e4b48d5cfdec6b337cbf193b4
Captain Throwback <captainthrowback@hotmail.com>
2020-02-18 23:31:39 -0500

    ocn: cleanup device files
    - Set up for TWRP-only tree
    - Updates for TWRP upstream
    - Script updates for decryption
    
    Change-Id: Ib8e7b29093bf9b12d4467af66281441bd5c2822a

--- omni_ocn.mk
+++ omni_ocn.mk
@@ -17,8 +17,8 @@
 # Sample: This is where we'd set a backup provider if we had one
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
-# Inherit TWRP device configuration
-$(call inherit-product-if-exists, device/htc/ocn/twrp_ocn.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -26,3 +26,6 @@ $(call inherit-product, vendor/omni/config/common.mk)
 PRODUCT_NAME := omni_ocn
 PRODUCT_BRAND := htc
 PRODUCT_DEVICE := ocn
+PRODUCT_MODEL := HTC U11
+PRODUCT_MANUFACTURER := HTC
+
